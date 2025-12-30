413ad6a1eb826a6cead1f32401797eab0f43116e
Alex Tilot <alextilot@gmail.com>
2025-12-30 09:48:04 -0600

    oppsie: remove

--- src/server-runner/steam/api_registry.py
+++
@@ -1,9 +0,0 @@
-from collections.abc import Mapping
-
-from server_runner.steam.api.games.palworld_api import PalWorldAPI
-from server_runner.steam.api.rest_api_base import RESTSteamServerAPI
-from server_runner.steam.steam_app_id import SteamAppID
-
-API_REGISTRY: Mapping[SteamAppID, type[RESTSteamServerAPI]] = {
-    SteamAppID.PALWORLD_DEDICATED_SERVER: PalWorldAPI,
-}
